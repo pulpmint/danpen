b05284d7649366b5b0e583de4e6384c1f98e3b1a
Manmohan Singh <pingmanmohan@gmail.com>
2021-03-21 19:23:32 +0530

    Delete: unwanted files

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Danpen</title>
+      </Head>
+
+      <main>
+        <h1>Danpen</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
