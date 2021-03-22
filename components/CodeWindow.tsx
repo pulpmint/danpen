fac4b77ecacb49f765e69ac7d3d2916ce4a9752e
Manmohan Singh <pingmanmohan@gmail.com>
2021-03-22 14:12:22 +0530

    Add: sample gradient in code

--- components/CodeWindow.tsx
+++ components/CodeWindow.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@chakra-ui/layout";
+import { Box, Container } from "@chakra-ui/layout";
 import { FC } from "react";
 import usePanelSettings from "../hooks/usePanelSettings";
 
@@ -10,25 +10,39 @@ const CodeWindow: FC = () => {
   };
 
   return (
-    <Container
-      maxWidth="container.md"
-      minHeight="md"
-      marginTop="32"
-      marginBottom="32"
-      marginLeft="auto"
-      marginRight="auto"
-      paddingLeft="4"
-      paddingRight="4"
-      paddingTop="2"
-      paddingBottom="2"
-      borderRadius="md"
-      backgroundColor={getBackgroundColor()}
-    >
-      <code>
-        <span style={{ opacity: 0.5 }}>{lineNumber ? `1. ` : null}</span>
-        console.log("Hello");
-      </code>
-    </Container>
+    <>
+      <Box
+        zIndex="-1"
+        position="fixed"
+        top="0"
+        bottom="0"
+        left="0"
+        right="0"
+        bgGradient="linear(to-br, #2F80ED 0%, #B2FFDA 100%)"
+      ></Box>
+
+      <Container
+        maxWidth="container.md"
+        minHeight="md"
+        marginTop="32"
+        marginBottom="32"
+        marginLeft="auto"
+        marginRight="auto"
+        paddingLeft="4"
+        paddingRight="4"
+        paddingTop="2"
+        paddingBottom="2"
+        borderRadius="md"
+        boxShadow="xl"
+        opacity="0.9"
+        backgroundColor={getBackgroundColor()}
+      >
+        <code>
+          <span style={{ opacity: 0.5 }}>{lineNumber ? `1. ` : null}</span>
+          console.log("Hello");
+        </code>
+      </Container>
+    </>
   );
 };
 
