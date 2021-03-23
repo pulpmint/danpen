d9688ee3a1c6267c7722d8729e2893316bc8d207
Manmohan Singh <pingmanmohan@gmail.com>
2021-03-24 00:53:35 +0530

    Add: font CDNs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,9 +8,13 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600&family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fira+Code&family=IBM+Plex+Mono&family=Roboto+Mono&family=Source+Code+Pro&family=Space+Mono&family=Ubuntu+Mono&display=swap"
+            rel="stylesheet"
+          ></link>
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
