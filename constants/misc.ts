be38444b19e66b16190a4cf65c8d4184b605cc99
Manmohan Singh <pingmanmohan@gmail.com>
2021-03-30 13:51:18 +0530

    Add: default code

--- constants/misc.ts
+++ constants/misc.ts
@@ -1,2 +1,3 @@
 export const CDN_LINK: string =
   "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.60.0/mode/%N/%N.min.js";
+export const DEF_CODE: string = 'const hello = () => { console.log("Hello World!") }';
