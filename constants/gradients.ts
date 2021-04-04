8ea31d42d86fa04dd7c137363c29e9bd5649ccf5
Manmohan Singh <pingmanmohan@gmail.com>
2021-04-04 14:24:34 +0530

    Update: gradients based on VIBGYOR

--- constants/gradients.ts
+++ constants/gradients.ts
@@ -1,5 +1,9 @@
 export const GRADIENTS: string[] = [
-  "linear(to-br, #F14658 0%, #DC2537 100%)",
+  "linear(to-br, #DF98FA 0%, #9055FF 100%)",
+  "linear(to-br, #402662 0%, #3900A6 100%)",
+  "linear(to-br, #0B63F6 0%, #003CC5 100%)",
   "linear(to-br, #00EACB 0%, #00D09B 100%)",
-  "linear(to-br, #54D2EF 0%, #2AA6DA 100%)"
+  "linear(to-br, #FFE174 0%, #FFBF40 100%)",
+  "linear(to-br, #FF5A09 0%, #DC2537 100%)",
+  "linear(to-br, #F14658 0%, #DC2537 100%)"
 ];
