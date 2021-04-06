5fa01fa63ab94d8c29c934db044fa7b91a0a030c
Manmohan Singh <pingmanmohan@gmail.com>
2021-04-06 15:36:14 +0530

    Add: about markdown

---
+++ constants/about.ts
@@ -0,0 +1,9 @@
+export const aboutMarkdown: string = `Hello there! 👋
+
+This started as a personal project inspired by [Carbon](https://carbon.now.sh/) & [Ray](https://ray.so/). I wanted to build something not too simple 😇 like Ray but not too complicated 😵 like Carbon. So I built something with a little bit of flexibility 💃. This is designed for people who write a lot of Medium articles & Speakers out there sharing their thoughts in conferences.
+
+I've used a lot of open source libraries & frameworks 📚 such as [Next.js](https://nextjs.org/), [CodeMirror](https://codemirror.net/), [Chakra UI](https://chakra-ui.com/), etc. Also thanks to [Anatolii Saienko](https://github.com/tsayen) for his [DOM to Image](https://github.com/tsayen/dom-to-image) script which actually helped a lot.
+
+I'll keep on adding new features & improving the project 🛠️. Feel free to reach out to me for any suggestions or any bugs that you face while using the application. I hope it helps your shitty 💩 little code to look beautiful ✨ AF.
+
+Build with no 💔 by Manmohan Singh 👨‍💻`;
