5992b52a976e6c3a0c754cfd7ca0f55253ecdbd5
Manmohan Singh <pingmanmohan@gmail.com>
2021-04-08 15:55:58 +0530

    Update: SEO tags & OG images

--- constants/seo.ts
+++ constants/seo.ts
@@ -2,3 +2,8 @@ export const SEO_TITLE: string =
   "Danpen - Take beautiful ✨ screenshots of your shitty 💩 little code";
 export const SEO_DESCRIPTION: string =
   "Create beautiful screenshots of your code snippets. Choose from a range of themes, hide or show the background, toggle between a dark and light window, change font styles and more. Customize it your way.";
+export const SEO_IMAGE: string = "/images/cover.png";
+export const SEO_KEYWORDS: string =
+  "create, code, screenshots, beautiful, generator, terminal , window";
+
+export const SEO_TWITTER_CREATOR: string = "@pulpmint";
