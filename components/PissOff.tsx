f0a0f1dd81e357ae2ab76ae786feb677dbf6d6f2
Manmohan Singh <pingmanmohan@gmail.com>
2021-10-16 02:27:20 +0530

    Update: mobile message & editor text

--- components/PissOff.tsx
+++ components/PissOff.tsx
@@ -1,18 +1,40 @@
-import { Container, Heading, Text } from "@chakra-ui/layout";
+import { Container, Heading, Link, Text } from "@chakra-ui/layout";
 import { FC } from "react";
+import DoodleAlert from "../assets/SVGs/DoodleAlert";
 import useWindowSize from "../hooks/useWindowSize";
 
 const PissOff: FC = () => {
   const { height } = useWindowSize();
 
   return (
-    <Container display="flex" flexDirection="column" justifyContent="center" height={height}>
-      <Heading mb="4">🛑 Stop right here!</Heading>
+    <Container
+      display="flex"
+      flexDirection="column"
+      textAlign="center"
+      alignItems="center"
+      justifyContent="center"
+      height={height}
+    >
+      <DoodleAlert size={0.4} />
+
+      <Heading my="4">Attention!</Heading>
+
+      <br />
 
       <Text>
-        No one writes code on their mobile phones 📱 so I didn't solve a problem that never existed.
-        Open you 💻 laptop. And yes, I'm lazy.
+        Please use a PC / tablet or switch to desktop mode for perfect
+        experience. No one writes code on their mobile phones anyway.
       </Text>
+
+      <br />
+
+      <Link
+        isExternal
+        href="https://github.com/pulpmint/danpen/"
+        color="blue.300"
+      >
+        Check the code on GitHub
+      </Link>
     </Container>
   );
 };
