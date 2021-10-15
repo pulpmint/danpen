741926e03216543243a6ad29d9df7e686f375181
Manmohan Singh <pingmanmohan@gmail.com>
2021-10-15 22:08:57 +0530

    Update: gradients to normal CSS syntax values

--- constants/gradients.ts
+++ constants/gradients.ts
@@ -1,11 +1,11 @@
 import { Gradient } from "../types/PanelSettings";
 
 export const GRADIENTS: Gradient[] = [
-  { name: "Virgin Violet", value: "linear(to-br, #DF98FA 0%, #9055FF 100%)" },
-  { name: "Indeginous Indigo", value: "linear(to-br, #402662 0%, #3900A6 100%)" },
-  { name: "Bubbly Blue", value: "linear(to-br, #0B63F6 0%, #003CC5 100%)" },
-  { name: "Gluey Green", value: "linear(to-br, #00EACB 0%, #00D09B 100%)" },
-  { name: "Yay Yellow", value: "linear(to-br, #FFE174 0%, #FFBF40 100%)" },
-  { name: "Only Orange", value: "linear(to-br, #FF5A09 0%, #DC2537 100%)" },
-  { name: "Radiant Red", value: "linear(to-br, #F14658 0%, #DC2537 100%)" }
+  { name: "Virgin Violet", value: "linear-gradient(to bottom right, #DF98FA 0%, #9055FF 100%)" },
+  { name: "Indeginous Indigo", value: "linear-gradient(to bottom right, #402662 0%, #3900A6 100%)" },
+  { name: "Bubbly Blue", value: "linear-gradient(to bottom right, #0B63F6 0%, #003CC5 100%)" },
+  { name: "Gluey Green", value: "linear-gradient(to bottom right, #00EACB 0%, #00D09B 100%)" },
+  { name: "Yay Yellow", value: "linear-gradient(to bottom right, #FFE174 0%, #FFBF40 100%)" },
+  { name: "Only Orange", value: "linear-gradient(to bottom right, #FF5A09 0%, #DC2537 100%)" },
+  { name: "Radiant Red", value: "linear-gradient(to bottom right, #F14658 0%, #DC2537 100%)" }
 ];
