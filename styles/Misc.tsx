a79b3e63be5a48c3594a92e9591d2337014bc71c
Manmohan Singh <pingmanmohan@gmail.com>
2021-11-19 18:56:22 +0530

    Update: thin scrollbars

--- styles/Misc.tsx
+++ styles/Misc.tsx
@@ -1,13 +1,39 @@
 import { FC } from "react";
+import { theme } from "../config/theme";
+import { useColorModeValue } from "@chakra-ui/color-mode";
 
 import "focus-visible/dist/focus-visible";
 
 const Misc: FC = () => {
+  // colors
+  const scrollbarColor = useColorModeValue(
+    theme.colors.gray[200],
+    theme.colors.gray[700]
+  );
+
   return (
     <style jsx global>{`
       .js-focus-visible :focus:not([data-focus-visible-added]) {
         outline: none;
         box-shadow: none;
+      }
+
+      * {
+        scrollbar-width: thin;
+        scrollbar-color: ${scrollbarColor} rgba(0, 0, 0, 0);
+      }
+
+      *::-webkit-scrollbar {
+        width: 4px;
+      }
+
+      *::-webkit-scrollbar-track {
+        background: rgba(0, 0, 0, 0);
+      }
+
+      *::-webkit-scrollbar-thumb {
+        background-color: ${scrollbarColor};
+        border-radius: 100px;
       }
     `}</style>
   );
