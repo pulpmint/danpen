3d5e8bc818f9c46f6e509448fcfb9015403244d0
Manmohan Singh <pingmanmohan@gmail.com>
2021-11-22 21:30:23 +0530

    Add: custom button

---
+++ components/CustomButton.tsx
@@ -0,0 +1,43 @@
+import { Button, ButtonProps } from "@chakra-ui/button";
+import { useColorModeValue } from "@chakra-ui/color-mode";
+import { FC } from "react";
+import { ICON_BACKGROUND, ICON_BACKGROUND_HOVER } from "../config/colors";
+import CustomTooltip from "./CustomTooltip";
+
+interface CustomButtonProps {
+  buttonProps?: ButtonProps;
+  label?: string;
+}
+
+const CustomButton: FC<CustomButtonProps> = ({
+  children,
+  label = "",
+  buttonProps
+}) => {
+  // colors
+  const iconBackground = useColorModeValue(
+    ICON_BACKGROUND.light,
+    ICON_BACKGROUND.dark
+  );
+  const iconBackgroundHover = useColorModeValue(
+    ICON_BACKGROUND_HOVER.light,
+    ICON_BACKGROUND_HOVER.dark
+  );
+
+  return (
+    <CustomTooltip label={label}>
+      <Button
+        variant="solid"
+        size="sm"
+        rounded="full"
+        bg={iconBackground}
+        _hover={{ backgroundColor: iconBackgroundHover }}
+        {...buttonProps}
+      >
+        {children}
+      </Button>
+    </CustomTooltip>
+  );
+};
+
+export default CustomButton;
