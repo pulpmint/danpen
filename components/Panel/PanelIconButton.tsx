6f2990c302d021fff390d41edab39b7ae1518003
Manmohan Singh <pingmanmohan@gmail.com>
2021-12-26 00:42:26 +0530

    Fix: no tooltip on disabled panel buttons

--- components/Panel/PanelIconButton.tsx
+++ components/Panel/PanelIconButton.tsx
@@ -1,5 +1,6 @@
 import { ButtonProps, Button } from "@chakra-ui/button";
 import { useColorModeValue } from "@chakra-ui/color-mode";
+import { Box } from "@chakra-ui/react";
 import { FC } from "react";
 import { ICON_BACKGROUND, ICON_BACKGROUND_HOVER } from "../../config/colors";
 import CustomTooltip from "../CustomTooltip";
@@ -26,19 +27,21 @@ const PanelIconButton: FC<PanelIconButtonProps> = ({
 
   return (
     <CustomTooltip label={label}>
-      <Button
-        variant="unstyled"
-        size="lg"
-        w="12"
-        h="12"
-        p="3"
-        rounded="2xl"
-        bg={iconBackground}
-        _hover={{ backgroundColor: iconBackgroundHover }}
-        {...buttonProps}
-      >
-        {children}
-      </Button>
+      <Box>
+        <Button
+          variant="unstyled"
+          size="lg"
+          w="12"
+          h="12"
+          p="3"
+          rounded="2xl"
+          bg={iconBackground}
+          _hover={{ backgroundColor: iconBackgroundHover }}
+          {...buttonProps}
+        >
+          {children}
+        </Button>
+      </Box>
     </CustomTooltip>
   );
 };
