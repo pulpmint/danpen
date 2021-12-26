e11e4343ebd94a4082bb3164c0256db180ba74a0
Manmohan Singh <pingmanmohan@gmail.com>
2021-12-26 19:00:05 +0530

    Update: SEO title - language

--- constants/seo.ts
+++ constants/seo.ts
@@ -1,5 +1,5 @@
 export const SEO_TITLE: string =
-  "Danpen - Take beautiful ✨ screenshots of your shitty 💩 little code";
+  "Danpen - Take beautiful ✨ screenshots of your code and share it anywhere 💜";
 export const SEO_DESCRIPTION: string =
   "Create beautiful screenshots of your code snippets. Choose from a range of themes, hide or show the background, toggle between a dark and light window, change font styles and more. Customize it your way.";
 export const SEO_IMAGE: string = "/images/cover.png";
